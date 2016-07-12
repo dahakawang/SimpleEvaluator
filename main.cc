7674db1ceb35212e64bb6cdfd65e928a3960b74d
David <david.kaiqiang.wang@gmail.com>
2016-07-12 21:21:23 +0800

    add values

--- main.cc
+++ main.cc
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <iterator>
 #include <stdexcept>
+#include <type_traits>
 
 std::string load_file(const std::string& file_path) {
     std::stringstream buffer_stream;
@@ -17,8 +18,91 @@ std::string load_file(const std::string& file_path) {
     return buffer_stream.str();
 }
 
+class value_t {
+public:
+    enum type_t { DECIMAL, INTEGER, STRING };
+
+    template<typename T>
+    value_t(T _v,
+            typename std::enable_if<std::is_integral<T>::value>::type* dummy = nullptr) {
+        _type = INTEGER;
+        _data.integer = _v;
+    }
+
+    template<typename T>
+    value_t(T _v,
+            typename std::enable_if<std::is_floating_point<T>::value>::type* dummy = nullptr) {
+        _type = DECIMAL;
+        _data.decimal = _v;
+    }
+
+    value_t(const std::string& _v) {
+        _type = STRING;
+        _data.str = _v;
+    }
+
+    value_t(const char* _v) {
+        _type = STRING;
+        _data.str = _v;
+    }
+
+
+    type_t type() const {
+        return _type;
+    }
+
+    long long get_integer() const {
+        return _data.integer;
+    }
+
+    double get_decimal() const {
+        return _data.decimal;
+    }
+
+    const std::string& get_string() const {
+        return _data.str;
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, const value_t& value);
+private:
+    type_t _type;
+    struct {
+        long long integer;
+        double decimal;
+        std::string str;
+    } _data;
+};
+
+std::ostream& operator<<(std::ostream& stream, const value_t& value) {
+    if (value._type == value_t::DECIMAL) {
+        stream << value.get_decimal();
+    } else if (value._type == value_t::INTEGER) {
+        stream << value.get_integer();
+    } else {
+        stream << value.get_string();
+    }
+
+    return stream;
+};
+
+
+class expression_t {
+public:
+    virtual value_t eval();
+};
+
+class const_expression_t : public expression_t {
+public:
+    value_t eval() override {
+        return _value;
+    }
+
+private:
+    value_t _value;
+};
+
 int main(int argc, char *argv[]) {
-    std::cout << load_file("smakefile") << std::endl;
-    std::cout << "ending" << std::endl;
+    value_t v("sdfasf");
+    std::cout << v << std::endl;
     return 0;
 }
